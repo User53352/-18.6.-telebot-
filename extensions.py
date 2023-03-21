b73f07f11571d7391a284af7a107bdb33232f2c2
Max <42574081+User53352@users.noreply.github.com>
2023-03-21 23:38:21 +0300

    Create extensions.py

---
+++ extensions.py
@@ -0,0 +1,31 @@
+import requests
+import json
+from config import values
+class APIException(Exception):
+    pass
+class CryptoConverter:
+    @staticmethod
+    def convert(base: str, quote: str, amount: str):
+        if base == quote:
+          raise APIException(f'Не удалось перевести одинаковые валюты: {base}-{quote}')
+
+        try:
+          base_ticker = values[base]
+        except KeyError:
+          raise APIException(f'Не удалось обработать валюту {base}')
+
+        try:
+            quote_ticker = values[quote]
+        except KeyError:
+          raise APIException(f'Не удалось обработать валюту {quote}')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+          raise APIException(f'Не удалось обработать количество {amount}')
+
+        r = requests.get(f'https://ru.investing.com/currencies/streaming-forex-rates-majors?fsym={base_ticker}&tsyms={quote_ticker}')
+        result = json.loads(r.content)[values[quote]]
+        result *= amount
+
+        return result
